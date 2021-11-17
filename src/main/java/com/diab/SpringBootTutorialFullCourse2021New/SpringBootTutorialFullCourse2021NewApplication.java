20bcd5a90fc0d12006229db51ac304465924d470
Mdiab-hub <motasem.diab@gmail.com>
2021-11-17 10:20:52 -0500

    first commit

---
+++ src/main/java/com/diab/SpringBootTutorialFullCourse2021New/SpringBootTutorialFullCourse2021NewApplication.java
@@ -0,0 +1,13 @@
+package com.diab.SpringBootTutorialFullCourse2021New;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTutorialFullCourse2021NewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTutorialFullCourse2021NewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diab/SpringBootTutorialFullCourse2021New/SpringBootTutorialFullCourse2021NewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diab.SpringBootTutorialFullCourse2021New;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTutorialFullCourse2021NewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
